3caa7ba83a63c0975aca77067db384e570df6cde
NedicNemanja <sdi1400124@di.uoa.gr>
2018-10-05 02:31:59 +0300

    Code cleanup. More commentary added. Started corner cases.

--- mpi/filter.c
+++ mpi/filter.c
@@ -4,52 +4,59 @@
 #include "mpi.h"
 #include "filter.h"
 
+//Normalized filter for blurring
 const float blurFilter[3][3] = {{1/16.0, 2/16.0, 1/16.0}, {2/16.0, 4/16.0, 2/16.0}, {1/16.0, 2/16.0, 1/16.0}};
 
+//Convolute every pixel fro source and write the results to destination. (s=1)
 void blur(unsigned char* source, unsigned char* dest, int startRow, int endRow, int startCol, int endCol, int width, int height, int isRGB) {
 	int i, j;
-    if (isRGB) {
-		for (i = startRow ; i <= endRow ; i++) {
-			for (j = startCol ; j <= endCol ; j++) {
+	/*note: the "if" is outside of the loops to increace efficency
+	(don't check the same condition for every loop)*/
+  if (isRGB) {
+		for (i = startRow; i <= endRow; i++) {
+			for (j = startCol; j <= endCol; j++) {
 				blurRGB(source, dest, i, j*3, width*3+6, height);
     	}
   	}
 	} else {
-		for (i = startRow ; i <= endRow ; i++) {
-			for (j = startCol ; j <= endCol ; j++) {
+		for (i = startRow; i <= endRow; i++) {
+			for (j = startCol; j <= endCol; j++) {
 				blurGrey(source, dest, i, j, width+2, height);
       }
     }
   }
 }
 
+/*Blur a 3x3 block around the selected pixel*/
 void blurGrey(unsigned char* source, unsigned char* dest, int x, int y, int width, int height) {
-	int i, j, k=0;
+	int i, j, p=0;
 	float cell = 0;
-	for (i = x-1; i <= x+1 ; i++) {
-        int m = 0;
-		for (j = y-1; j <= y+1 ; j++) {
-			cell += source[width * i + j] * blurFilter[k][m];
-            m++;
-        }
-        k++;
+	for (i = x-1; i <= x+1; i++) {	//x axis
+    int q = 0;
+		for (j = y-1; j <= y+1; j++) {	//y axis
+			cell += source[width * i + j] * blurFilter[p][q];	//apply fliter
+      q++;
     }
-	dest[width * x + y] = cell;
+    p++;
+  }
+	dest[width * x + y] = cell;	//commit the result
 }
 
+/*Blur a 3x3 block around the selected pixel for every color separately.*/
 void blurRGB(unsigned char* source, unsigned char* dest, int x, int y, int width, int height) {
-	int i, j, k=0;
+	int i, j, p=0;
 	float red = 0, green = 0, blue = 0;
-	for (i = x-1; i <= x+1 ; i++) {
-        int m = 0;
-		for (j = y-3; j <= y+3 ; j+=3) {
-			red += source[width * i + j]* blurFilter[k][m];
-			green += source[width * i + j+1] * blurFilter[k][m];
-			blue += source[width * i + j+2] * blurFilter[k][m];
-            m++;
+	for (i = x-1; i <= x+1 ; i++) {	//x axis
+    int q = 0;
+		for (j = y-3; j <= y+3 ; j+=3) {	//y axis
+			red += source[width * i + j]* blurFilter[p][q];
+			green += source[width * i + j+1] * blurFilter[p][q];
+			blue += source[width * i + j+2] * blurFilter[p][q];
+      q++;
 		}
-        k++;
-    }
+    p++;
+  }
+	//commit results
 	dest[width * x + y] = red;
 	dest[width * x + y+1] = green;
 	dest[width * x + y+2] = blue;
--- mpi/main.c
+++ mpi/main.c
@@ -9,11 +9,13 @@
 
 MPI_Status status;
 
+//contiguous datatypes for communicating border rows & cols
 MPI_Datatype rowGrey;
 MPI_Datatype rowRGB;
 MPI_Datatype colGrey;
 MPI_Datatype colRGB;
 
+//mpi requests for sharing border pixels
 MPI_Request topSend;
 MPI_Request bottomSend;
 MPI_Request leftSend;
@@ -42,15 +44,18 @@ int main(int argc, char** argv) {
 	  char *imageName;
     int isRGB,comm_rank, comm_size;
     double comm_size_sqrt;
+    //Init MPI enviroment and share arguments between processes
     MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &comm_size);  //size of the group associated with a communicator
-    MPI_Comm_rank(MPI_COMM_WORLD, &comm_rank);  //rank of the calling process in the communicator
+    //size of the group associated with a communicator
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
+    //rank of the calling process in the communicator
+    MPI_Comm_rank(MPI_COMM_WORLD, &comm_rank);
 
     /* check input arguments */
     if (comm_rank == 0) {
         if (argc != 6) {
             fprintf(stderr, "Bad input provided\n");
-            MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);  //terminate all tasks in comm
+            MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);  //end all tasks in comm
             exit(EXIT_FAILURE);
         }
     }
@@ -62,44 +67,50 @@ int main(int argc, char** argv) {
     loops = atoi(argv[5]);
     /*check arguments for compliance*/
     if(comm_rank == 0) {
-        comm_size_sqrt = sqrt(comm_size);
-        if ((comm_size_sqrt_int = (int) comm_size_sqrt) != comm_size_sqrt ||
-            width % comm_size_sqrt_int != 0 ||
-            height % comm_size_sqrt_int != 0) {
-            fprintf(stderr, "Bad config provided\n");
-            MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
-            exit(EXIT_FAILURE);
-        } else {
-            splitRowNum = comm_size_sqrt_int;
-            splitColNum = comm_size_sqrt_int;
-        }
+      comm_size_sqrt = sqrt(comm_size);
+      if ((comm_size_sqrt_int = (int) comm_size_sqrt) != comm_size_sqrt ||
+        width % comm_size_sqrt_int != 0 ||
+        height % comm_size_sqrt_int != 0) {
+        fprintf(stderr, "Bad config provided\n");
+        MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+        exit(EXIT_FAILURE);
+      } else {
+        splitRowNum = comm_size_sqrt_int;
+        splitColNum = comm_size_sqrt_int;
+      }
     }
-    printf("arg %s %s %s %s %s\n", argv[1], argv[2], argv[3], argv[4], argv[5]);
-    printf("int %d %s %d %d %d\n", isRGB, imageName, width, height, loops );
 
-    //broadcast info from the rank=0 process to all other
-    broadcastInfo(&splitRowNum, &splitColNum, &comm_size_sqrt_int);
+  //broadcast info from the rank=0 process to all other(MAY BE UNNECESSARY-overhead?)
+  broadcastInfo(&splitRowNum, &splitColNum, &comm_size_sqrt_int);
 
-    rows = height / splitRowNum;
-    cols = width / splitColNum;
+  rows = height / splitRowNum;
+  cols = width / splitColNum;
 
-    MPI_Type_contiguous(cols, MPI_BYTE, &rowGrey);
-  	MPI_Type_commit(&rowGrey);
-  	MPI_Type_contiguous(3*cols, MPI_BYTE, &rowRGB);
-  	MPI_Type_commit(&rowRGB);
-  	MPI_Type_vector(rows, 1, cols+2, MPI_BYTE, &colGrey);
-  	MPI_Type_commit(&colGrey);
-  	MPI_Type_vector(rows, 3, 3*cols+6, MPI_BYTE, &colRGB);
-  	MPI_Type_commit(&colRGB);
+  //Create contiguous types for rows & cols
+  /*note: MPI_Type_contiguous is for making a new datatype which is count copies
+          of the existing one. This is useful to simplify the processes of
+          sending a number of datatypes together as you don't need to keep track
+          of their combined size.*/
+  MPI_Type_contiguous(cols, MPI_BYTE, &rowGrey);
+  MPI_Type_commit(&rowGrey);
+  MPI_Type_contiguous(3*cols, MPI_BYTE, &rowRGB);
+  MPI_Type_commit(&rowRGB);
+  /*Columns need a vector type with a stride between pixles
+  equal to the width of the array (=cols+2)*/
+  MPI_Type_vector(rows, 1, cols+2, MPI_BYTE, &colGrey);
+  MPI_Type_commit(&colGrey);
+  MPI_Type_vector(rows, 3, 3*(cols+2), MPI_BYTE, &colRGB);
+  MPI_Type_commit(&colRGB);
 
-    //calculate the starting row/col
-    int start_row = (comm_rank / comm_size_sqrt_int) * rows;
-    int start_col = (comm_rank % comm_size_sqrt_int) * cols;
+  //calculate the starting row/col
+  int start_row = (comm_rank / comm_size_sqrt_int) * rows;
+  int start_col = (comm_rank % comm_size_sqrt_int) * cols;
 
-  //allocate space for the file
+  /*Allocate space for the arrays but with a 1pixel border around them:
+    source: the array which we read from
+    destination: here we store the convolution results after every loop*/
 	unsigned char* source = NULL;
   unsigned char* dest = NULL;
-	MPI_File mpi_file;
 	int filesize = fileMemAllocSize(rows, cols, isRGB);
 	source = calloc(filesize, sizeof(unsigned char));
 	dest = calloc(filesize, sizeof(unsigned char));
@@ -109,90 +120,91 @@ int main(int argc, char** argv) {
         return EXIT_FAILURE;
 	}
 
-	// Parallel read the file to the source array
+	/*Read the process block from file to the source array row by row,
+  filling only the inner pixels, not the border ones.*/
+  MPI_File mpi_file;
 	MPI_File_open(MPI_COMM_WORLD, imageName, MPI_MODE_RDONLY, MPI_INFO_NULL, &mpi_file);
 	if (isRGB) {
-        int i;
+    int i;
 		for (i = 1 ; i <= rows ; i++) {
-			MPI_File_seek(mpi_file, 3*(start_row + i-1) * width + 3*start_col, MPI_SEEK_SET);
-			MPI_File_read(mpi_file, &source[i*(cols*3+6)+3], cols*3, MPI_BYTE, &status);
+			MPI_File_seek(mpi_file, ((start_row + i-1) * width + start_col)*3, MPI_SEEK_SET);
+			MPI_File_read(mpi_file, &source[(i*(cols+2)+1)*3], cols*3, MPI_BYTE, &status);
 		}
 	} else {
-        int i;
-        for (i = 1 ; i <= rows ; i++) {
+    int i;
+    for (i = 1 ; i <= rows ; i++) {
 			MPI_File_seek(mpi_file, (start_row + i-1) * width + start_col, MPI_SEEK_SET);
 			MPI_File_read(mpi_file, &source[i*(cols+2)+1], cols, MPI_BYTE, &status);
 		}
 	}
 	MPI_File_close(&mpi_file);
 
-  //find neighbor blocks
-    int top, bottom, left, right;
-    if (start_row != 0) {
-        top = comm_rank - splitColNum;
-    } else {  //first line blocks have no top
-        top = -1;
-    }
-    if (start_row + rows != height) {
-        bottom = comm_rank + splitColNum;
-    } else {  //last line blocks have no bottom
-        bottom = -1;
-    }
-    if (start_col != 0) {
-        left = comm_rank - 1;
-    } else { //left column blocks have no left
-        left = -1;
-    }
-    if (start_col + cols != width) {
-        right = comm_rank + 1;
-    } else {  //right column blocks have no right
-        right = -1;
-    }
+  //Find neighbor processes/blocks so we can request border rows & cols
+  int top=-1, bottom=-1, left=-1, right=-1,
+      top_left=-1,top_right=-1,bot_left=-1, bot_right=-1;
+  if (start_row != 0)
+      top = comm_rank - splitColNum;
+  if (start_row + rows != height)
+    bottom = comm_rank + splitColNum;
+  if (start_col != 0)
+    left = comm_rank - 1;
+  if (start_col + cols != width)
+    right = comm_rank + 1;
+  //Find corner processes/blocks
+  if(top != -1 && left != -1)
+    top_left = top - 1;
+  if(top != -1 && right != -1)
+    top_right = top + 1;
+  if(bottom != -1 && left !=- 1)
+    bot_left = bot - 1;
+  if(bottom != -1 && right != -1)
+    bot_right = bot + 1;
 
-	MPI_Barrier(MPI_COMM_WORLD); //block until all processes reach this routine
+	MPI_Barrier(MPI_COMM_WORLD); //block until all processes know their neighbor
 
   double startTime = MPI_Wtime();
 	// Convolute "loops" times
 	for (t = 0 ; t < loops ; t++) {
-        /* Send and request borders */
+    /* Send and request borders (non-blocking)*/
 		if (isRGB) {
-			if (top != -1) { //not topmost block
-				MPI_Isend(&source[3*cols+6 + 3], 1, rowRGB, top, 0, MPI_COMM_WORLD, &topSend);
+			if (top != -1) { //top border
+				MPI_Isend(&source[3*(cols+2 + 1)], 1, rowRGB, top, 0, MPI_COMM_WORLD, &topSend);
 				MPI_Irecv(&source[3], 1, rowRGB, top, 0, MPI_COMM_WORLD, &topRcv);
 			}
-			if (bottom != -1) {  //not bottommost block
-				MPI_Isend(&source[rows*(3*cols+6) + 3], 1, rowRGB, bottom, 0, MPI_COMM_WORLD, &bottomSend);
+			if (bottom != -1) {  //bottom border
+				MPI_Isend(&source[(rows*(cols+2) + 1)*3], 1, rowRGB, bottom, 0, MPI_COMM_WORLD, &bottomSend);
 				MPI_Irecv(&source[(rows+1)*(3*cols+6) + 3], 1, rowRGB, bottom, 0, MPI_COMM_WORLD, &bottomRcv);
 			}
-			if (left != -1) { //not leftmost block
-                MPI_Isend(&source[3*cols+6 + 3], 1, colRGB, left, 0, MPI_COMM_WORLD, &leftSend);
-                MPI_Irecv(&source[3*cols+6], 1, colRGB, left, 0, MPI_COMM_WORLD, &leftRcv);
+			if (left != -1) { //left border
+        MPI_Isend(&source[3*cols+6 + 3], 1, colRGB, left, 0, MPI_COMM_WORLD, &leftSend);
+        MPI_Irecv(&source[3*cols+6], 1, colRGB, left, 0, MPI_COMM_WORLD, &leftRcv);
 			}
-			if (right != -1) { //not rightmost block
-                MPI_Isend(&source[3*cols+6 + 3*cols], 1, colRGB, right, 0, MPI_COMM_WORLD, &rightSend);
-                MPI_Irecv(&source[3*cols+6 + 3*cols+3], 1, colRGB, right, 0, MPI_COMM_WORLD, &rightRcv);
+			if (right != -1) { //right border
+        MPI_Isend(&source[3*cols+6 + 3*cols], 1, colRGB, right, 0, MPI_COMM_WORLD, &rightSend);
+        MPI_Irecv(&source[3*cols+6 + 3*cols+3], 1, colRGB, right, 0, MPI_COMM_WORLD, &rightRcv);
 			}
 		} else {
-            if (top != -1) {  //not first block
-				MPI_Isend(&source[cols+3], 1, rowGrey, top, 0, MPI_COMM_WORLD, &topSend);
+      if (top != -1) {  //top border
+				MPI_Isend(&source[cols+2 + 1], 1, rowGrey, top, 0, MPI_COMM_WORLD, &topSend);
 				MPI_Irecv(&source[1], 1, rowGrey, top, 0, MPI_COMM_WORLD, &topRcv);
 			}
-			if (bottom != -1) { //not last block
+			if (bottom != -1) { //bottom border
 				MPI_Isend(&source[rows*(cols+2) + 1], 1, rowGrey, bottom, 0, MPI_COMM_WORLD, &bottomSend);
 				MPI_Irecv(&source[(rows+1)*(cols+2)+1], 1, rowGrey, bottom, 0, MPI_COMM_WORLD, &bottomRcv);
 			}
-            if (left != -1) { //not leftmost block
-                MPI_Isend(&source[cols+2 + 1], 1, colGrey, left, 0, MPI_COMM_WORLD, &leftSend);
-                MPI_Irecv(&source[cols+2], 1, colGrey, left, 0, MPI_COMM_WORLD, &leftRcv);
-            }
-            if (right != -1) { //not rightmost block
-                MPI_Isend(&source[cols+2 + cols], 1, colGrey, right, 0, MPI_COMM_WORLD, &rightSend);
-                MPI_Irecv(&source[cols+2 + cols+1], 1, colGrey, right, 0, MPI_COMM_WORLD, &rightRcv);
-            }
+      if (left != -1) { //left border
+        MPI_Isend(&source[cols+2 + 1], 1, colGrey, left, 0, MPI_COMM_WORLD, &leftSend);
+        MPI_Irecv(&source[cols+2], 1, colGrey, left, 0, MPI_COMM_WORLD, &leftRcv);
+      }
+      if (right != -1) { //right border
+        MPI_Isend(&source[cols+2 + cols], 1, colGrey, right, 0, MPI_COMM_WORLD, &rightSend);
+        MPI_Irecv(&source[cols+2 + cols+1], 1, colGrey, right, 0, MPI_COMM_WORLD, &rightRcv);
+      }
 		}
 
+    //Covolute internal pixels (not the border ones!)
 		blur(source, dest, 1, rows, 1, cols, cols, rows, isRGB);
-
+    //Wait to receive and convolute border pixels
 		if (top != -1) {
 			MPI_Wait(&topRcv, &status);
 			blur(source, dest, 1, 1, 2, cols-1, cols, rows, isRGB);
@@ -202,14 +214,15 @@ int main(int argc, char** argv) {
 			blur(source, dest, rows, rows, 2, cols-1, cols, rows, isRGB);
 		}
 		if (left != -1) {
-		    MPI_Wait(&leftRcv, &status);
-            blur(source, dest, 2, rows-1, 1, 1, cols, rows, isRGB);
+		  MPI_Wait(&leftRcv, &status);
+      blur(source, dest, 2, rows-1, 1, 1, cols, rows, isRGB);
 		}
 		if (right != -1) {
-		    MPI_Wait(&rightRcv, &status);
-            blur(source, dest, 2, rows-1, cols, cols, cols, rows, isRGB);
+		  MPI_Wait(&rightRcv, &status);
+      blur(source, dest, 2, rows-1, cols, cols, cols, rows, isRGB);
 		}
 
+    //Convolute corner pixels (MAYBE WAIT HERE FOR CORNERS)
 		if (top != -1 && left != -1) {
 		    blur(source, dest, 1, 1, 1, 1, cols, rows, isRGB);
 		}
@@ -223,23 +236,25 @@ int main(int argc, char** argv) {
 		    blur(source, dest, rows, rows, cols, cols, cols, rows, isRGB);
 		}
 
-		if (top != -1) {
-            MPI_Wait(&topSend, &status);
-        }
-		if (bottom != -1) {
-            MPI_Wait(&bottomSend, &status);
-        }
-		if (left != -1) {
-            MPI_Wait(&leftSend, &status);
-        }
-        if (right != -1) {
-            MPI_Wait(&rightSend, &status);
-        }
+    unsigned char* temp;
+    temp = source;
+    source = dest;
+    dest = temp;
 
-        unsigned char* temp;
-		temp = source;
-        source = dest;
-        dest = temp;
+    //wait until you sent all borders
+		if (top != -1) {
+      MPI_Wait(&topSend, &status);
+    }
+		if (bottom != -1) {
+      MPI_Wait(&bottomSend, &status);
+    }
+		if (left != -1) {
+      MPI_Wait(&leftSend, &status);
+    }
+    if (right != -1) {
+      MPI_Wait(&rightSend, &status);
+    }
+
 	}
     double totalTime = MPI_Wtime() - startTime;
 
